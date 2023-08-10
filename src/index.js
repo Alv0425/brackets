7804f2afa13f4b455b1bbf001d4202a5204a5322
Alv0425 <vilejshikova@gmail.com>
2023-08-10 16:42:01 +0300

    feat: construct regexps from bracketsConfig

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function check(str, bracketsConfig) {
-  // start bracket task
+  //construct regexps for brackets configurations
+  let brackets = bracketsConfig.map(bracket => (`\\${bracket[0]}\\${bracket[1]}`));
+  let regexps = brackets.map(brackettype => new RegExp(brackettype,'g'));
 }
