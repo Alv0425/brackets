7a4408874040748b48835d074f1c4f0953020cfb
Alv0425 <vilejshikova@gmail.com>
2023-08-10 17:03:38 +0300

    feat: correct regrexps and add solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
+module.exports = function check(expr, bracketsConfig) {
   //construct regexps for brackets configurations
-  let brackets = bracketsConfig.map(bracket => (`\\${bracket[0]}\\${bracket[1]}`));
+  let brackets = bracketsConfig.map(bracket => {
+    if (/\d/.test(bracket[0])) {
+      return `${bracket[0]}${bracket[1]}`;
+    }
+    return `\\${bracket[0]}\\${bracket[1]}`;
+  });
   let regexps = brackets.map(brackettype => new RegExp(brackettype,'g'));
+  //check all configuration while exp contains '()', '{}', '[]', ...
+  let test = [];
+  do {
+    for (let i = 0; i < regexps.length; i++){
+      test[i] = regexps[i].test(expr);
+      if (test[i]) {
+        expr = expr.replace(regexps[i],'');
+      }
+    }
+  } while (test.includes(true)) 
+  //return true when exp is empty and false otherwise
+  return expr == '';
 }
